85328f31afef37162d7b09f5b4e9b709730c2c59
Tishya Chhabra <tatooine888@gmail.com>
2020-04-20 15:57:23 -0700

    updated climb

--- questions/ClimbingClub.java
+++ questions/ClimbingClub.java
@@ -11,13 +11,53 @@ public class ClimbingClub {
         climbList = new ArrayList<ClimbInfo>();
     }
 
-    public void addClimb(String peakName, int climbTime){
+
+    /**
+     * Write an implementation of the ClimbingClub method addClimb that stores the ClimbInfo 
+     * objects in the order they were added. This implementation of addClimb should create a new 
+     * ClimbInfo object with the given name and time. It appends a reference to that object to the 
+     * end of climbList. 
+     */
+    public void addClimb1(String peakName, int climbTime){
         //your code here
     }
 
-    public int distinctPeakNames(){
+    /**
+     * Write an implementation of the ClimbingClub method addClimb that stores the elements of 
+     * climbList in alphabetical order by name (as determined by the compareTo method of the 
+     * String class). This implementation of addClimb should create a new ClimbInfo object with the 
+     * given name and time and then insert the object into the appropriate position in climbList. 
+     * Entries that have the same name will be grouped together and can appear in any order within 
+     * the group. 
+     */
+    public void addClimb2(String peakName, int climbTime){
         //your code here
-        return 0;
+    }
+
+
+    /**
+     * Consider the following implementation of the method below
+     * Which is supposed to return the number of different names in 'climbList'
+     */
+    public int distinctPeakNames(){
+        if(climbList.size() == 0){
+            return 0;
+        }
+        ClimbInfo currInfo = climbList.get(0);
+        String prevName = currInfo.getName();
+        String currName = null;
+        int numNames = 1;
+
+        for(int k = 1; k < climbList.size(); k++){
+            currInfo = climbList.get(k);
+            currName = currInfo.getName();
+            if(prevName.compareTo(currName) != 0){
+                numNames++;
+                prevName = currName;
+            }
+        }
+
+        return numNames;
     }
 
 }
--- solutions/ClimbingClub.java
+++ solutions/ClimbingClub.java
@@ -11,10 +11,29 @@ public class ClimbingClub {
         climbList = new ArrayList<ClimbInfo>();
     }
 
-    public void addClimb(String peakName, int climbTime){
+    public void addClimb1(String peakName, int climbTime){
         ClimbInfo newClimb = new ClimbInfo(peakName, climbTime);
         climbList.add(newClimb);
     }
+
+    public void addClimb2(String peakName, int climbTime){
+        ClimbInfo newClimb = new ClimbInfo(peakName, climbTime);
+        String otherName;
+        
+        if(climbList.size() == 0) climbList.add(newClimb);
+        else{
+            for(int i = 0; i < climbList.size(); i++){
+                otherName = climbList.get(i).getName();
+
+                if(peakName.compareTo(otherName) <= 0){
+                    climbList.add(i-1, newClimb);
+                    break;
+                }
+            }
+
+        }
+    }
+
 
     public int distinctPeakNames(){
         //your code here
