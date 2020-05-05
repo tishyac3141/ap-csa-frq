4076881fba939220566c1be1fcc2a6e62f3e7371
Tishya Chhabra <tatooine888@gmail.com>
2020-05-04 21:57:55 -0700

    worked on gameSpinner

--- solutions/GameSpinner.java
+++ solutions/GameSpinner.java
@@ -1,5 +1,43 @@
 package solutions;
 
 public class GameSpinner {
+
+    private int sectors;
+    private int amtOfRuns = 0;
+    private int previousNum = 0;
+
+    public GameSpinner(int sectors){
+        this.sectors = sectors;
+    }
+    
+    public int currentRun(){
+        return amtOfRuns;
+    }
+
+    public int spin(){
+
+        int num = (int)((Math.random() * 4) + 1);
+
+        if(previousNum == 0){
+            amtOfRuns = 1;
+            previousNum = num;
+            return num;
+        } 
+        else if(num == previousNum){
+            amtOfRuns++;
+            previousNum = num;
+            return num;
+        } 
+        else {
+            previousNum = num;
+            amtOfRuns = 1;
+            return num;
+        }
+    }
+
+    public static void main(String[] args){
+        GameSpinner spinner = new GameSpinner(4);
+        System.out.println(spinner.spin());
+    }
     
 }
