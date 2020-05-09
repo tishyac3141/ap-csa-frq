ae6069c5e76bff8b0efc5a8153f7227ac19ef388
Tishya Chhabra <tatooine888@gmail.com>
2020-05-08 20:59:55 -0700

    finished WordList

---
+++ questions/WordList.java
@@ -0,0 +1,18 @@
+package questions;
+
+import java.util.ArrayList;
+
+public class WordList {
+
+    private ArrayList myList;
+
+    public int numWordsOfLength(int len){
+        //your code here
+        return 0;
+    }
+
+    public void removeWordsOfLength(int len){
+        //your code here
+    }
+    
+}
---
+++ solutions/WordList.java
@@ -0,0 +1,34 @@
+package solutions;
+
+import java.util.ArrayList;
+
+public class WordList {
+
+    private ArrayList myList;
+
+    public int numWordsOfLength(int len){
+        int num = 0;
+
+        String word;   
+        for(int i = 0; i < myList.size(); i++){
+           word = (String) myList.get(i);
+           if(word.length() == len) num++;
+            
+        }
+
+        return num;
+    }
+
+    public void removeWordsOfLength(int len){
+        String word;
+        for(int i = 0; i < myList.size(); i++){
+            word = (String) myList.get(i);
+            if(word.length() == len) {
+                myList.remove(i);
+                i--;
+            }
+             
+         }
+    }
+    
+}
