d6e5a5646c99dc43f2bd803ec965a087a11c33e9
Tishya Chhabra <tatooine888@gmail.com>
2020-05-09 23:52:55 -0700

    created StringCoder

---
+++ solutions/StringCoder.java
@@ -0,0 +1,38 @@
+package solutions;
+
+import java.util.ArrayList;
+
+public class StringCoder {
+
+    private String masterString;
+
+    public StringCoder(String master){
+        masterString = master;
+    }
+
+    public String decodeString(ArrayList<StringPart> parts){
+        String fullString = "";
+        StringPart part;
+        for(int i = 0; i < parts.size(); i++){
+            part = parts.get(i);
+            fullString += masterString.substring(part.getStart(), part.getStart() + part.getLength()) + 1;
+        }
+
+        return fullString;
+    }
+
+    public ArrayList<StringPart> encodeString(String word){
+        ArrayList<StringPart> parts = new ArrayList<StringPart>();
+
+        int length = 0;
+        
+
+        return parts;
+
+    }
+
+    private StringPart findPart(String str){
+        return null;
+    }
+    
+}
---
+++ solutions/StringPart.java
@@ -0,0 +1,20 @@
+package solutions;
+
+public class StringPart{
+
+    private int start;
+    private int length;
+
+    public StringPart(int start, int length){
+        this.start = start;
+        this.length = length;
+    }
+
+    public int getStart(){
+        return start;
+    }
+
+    public int getLength(){
+        return length;
+    }
+}
