7134fe246ac6b54d577032cc4361c9ec3d7a49a2
Tishya Chhabra <tatooine888@gmail.com>
2020-05-14 13:53:03 -0700

    created and finished Digits class

---
+++ solutions/Digits.java
@@ -0,0 +1,32 @@
+package solutions;
+
+import java.util.ArrayList;
+
+public class Digits {
+
+    public ArrayList<Integer> digitList;
+
+    public Digits(int num){
+        int digit;
+        digitList = new ArrayList<Integer>();
+
+        while(num > 0){
+            digit = num % 10;
+            digitList.add(0, digit);
+            num = num / 10;
+        }
+    }
+
+    public boolean isStrictlyIncreasing(){
+        boolean isIncreasing = true;
+        if(digitList.size() == 1) return true;
+        for(int i = 1; i < digitList.size(); i++){
+            if(digitList.get(i) <= digitList.get(i - 1)) isIncreasing = false;
+        }
+
+        return isIncreasing;
+    }
+
+    
+    
+}
