52083826e9c05db700f8f2432d0e979d318d3e13
Tishya Chhabra <tatooine888@gmail.com>
2020-05-20 09:17:52 -0700

    created questions

---
+++ questions/BatteryCharger.java
@@ -0,0 +1,19 @@
+package questions;
+
+/**
+ * STAND ALONE QUESTION
+ */
+
+public class BatteryCharger {
+
+    private int getChargingCost(int startHour, int chargeTime){
+        //your code here
+        return 0;
+    }
+
+    public int getChargeStartTime(int chargeTime){
+        //your code here
+        return 0;
+    }
+    
+}
---
+++ questions/SparseArrayEntry.java
@@ -0,0 +1,29 @@
+package questions;
+
+/**
+ * SUPPLEMENTARY 
+ */
+
+public class SparseArrayEntry{
+    private int row;
+    private int col;
+    private int value;
+
+    public SparseArrayEntry(int r, int c, int v){
+        row = r;
+        col = c;
+        value = v;
+    }
+
+    public int getRow(){
+        return row;
+    }
+
+    public int getCol(){
+        return col;
+    }
+
+    public int getValue(){
+        return value;
+    }
+}
